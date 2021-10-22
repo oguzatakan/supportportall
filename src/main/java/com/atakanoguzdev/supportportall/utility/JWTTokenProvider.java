07a25dc7aa21bdb8dac49c9365382713f8103929
Atakan <atakan17171717@hotmail.com>
2021-10-22 17:57:06 +0300

    jwttokenoprovider created

---
+++ src/main/java/com/atakanoguzdev/supportportall/utility/JWTTokenProvider.java
@@ -0,0 +1,68 @@
+package com.atakanoguzdev.supportportall.utility;
+
+import com.atakanoguzdev.supportportall.constant.SecurityConstant;
+import static com.atakanoguzdev.supportportall.constant.SecurityConstant.*;
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import static java.util.Arrays.stream;
+
+
+import com.atakanoguzdev.supportportall.domain.UserPrincipal;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class JWTTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateJwt(UserPrincipal userPrincipal){
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create().withIssuer(GET_ARRAYS_LLC).withAudience(GET_ARRAYS_ADMINISTRATION)
+                .withIssuedAt(new Date()) .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims).withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(HMAC512(secret.getBytes()));
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token){
+        String[] claims = getClaimsFromToken(token);
+        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try{
+            Algorithm algorithm = HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(GET_ARRAYS_LLC).build();
+        }catch (JWTVerificationException exception) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal user) {
+
+        List<String> authorities = new ArrayList<>();
+
+        for (GrantedAuthority grantedAuthority : user.getAuthorities()){
+            authorities.add(grantedAuthority.getAuthority());
+        }
+        return authorities.toArray(new String[0]);
+    }
+}
