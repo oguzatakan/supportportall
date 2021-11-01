6f20c43bfcc7f7528356759124f82b4f6c216a1b
Atakan <atakan17171717@hotmail.com>
2021-11-01 22:39:52 +0300

    authorization started

---
+++ src/main/java/com/atakanoguzdev/supportportall/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,38 @@
+package com.atakanoguzdev.supportportall.filter;
+
+import com.atakanoguzdev.supportportall.constant.SecurityConstant;
+import com.atakanoguzdev.supportportall.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.atakanoguzdev.supportportall.constant.SecurityConstant.*;
+import static org.springframework.http.HttpStatus.OK;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    private JWTTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JWTTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase(OPTION_HTTP_METHOD)) {
+            response.setStatus(OK.value());
+        } else {
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if (authorizationHeader == null || authorizationHeader.startsWith(TOKEN_PREFIX)) {
+                filterChain.doFilter(request,response);
+                return;
+            }
+        }
+
+    }
+}
