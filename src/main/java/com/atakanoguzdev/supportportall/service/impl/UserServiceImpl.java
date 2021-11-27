df2f2da34545dac1ad2a3c95c82d54db76313cc4
Atakan <atakan17171717@hotmail.com>
2021-11-27 15:19:49 +0300

    service and serviceımpl created

---
+++ src/main/java/com/atakanoguzdev/supportportall/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.atakanoguzdev.supportportall.service.impl;
+
+import com.atakanoguzdev.supportportall.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceImpl implements UserService, UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
