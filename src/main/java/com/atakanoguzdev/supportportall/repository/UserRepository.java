47a22f1335df0150a239481266bba671fcc16d8b
Atakan <atakan17171717@hotmail.com>
2021-11-29 19:44:33 +0300

    son değişiklikler

--- src/main/java/com/atakanoguzdev/supportportall/repository/UserRepository.java
+++ src/main/java/com/atakanoguzdev/supportportall/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.atakanoguzdev.supportportall.repository;
 
 import com.atakanoguzdev.supportportall.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findUserByUsername(String username);
+    User findUserByUserName(String username);
 
     User findUserByEmail(String email);
 }
--- src/main/java/com/atakanoguzdev/supportportall/service/UserService.java
+++ src/main/java/com/atakanoguzdev/supportportall/service/UserService.java
@@ -1,4 +1,7 @@
 package com.atakanoguzdev.supportportall.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
 }
