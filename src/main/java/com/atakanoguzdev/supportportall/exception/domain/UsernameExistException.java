6d4330f5898fab9a6a368c4258b9355202a2aa91
Atakan <atakan17171717@hotmail.com>
2021-11-30 19:30:15 +0300

    exception types created

---
+++ src/main/java/com/atakanoguzdev/supportportall/exception/domain/UsernameExistException.java
@@ -0,0 +1,7 @@
+package com.atakanoguzdev.supportportall.exception.domain;
+
+public class UsernameExistException extends Exception {
+    public UsernameExistException(String message) {
+        super(message);
+    }
+}
