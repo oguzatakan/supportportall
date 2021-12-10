736c1842e9bdbe9aca39afe0cf6f6d3c1df1e1b2
Atakan <atakan17171717@hotmail.com>
2021-12-10 18:56:35 +0300

    post login created

--- src/main/java/com/atakanoguzdev/supportportall/resource/UserResource.java
+++ src/main/java/com/atakanoguzdev/supportportall/resource/UserResource.java
@@ -1,24 +1,55 @@
 package com.atakanoguzdev.supportportall.resource;
 
 import com.atakanoguzdev.supportportall.domain.User;
+import com.atakanoguzdev.supportportall.domain.UserPrincipal;
 import com.atakanoguzdev.supportportall.exception.ExceptionHandling;
 import com.atakanoguzdev.supportportall.exception.domain.EmailExistException;
 import com.atakanoguzdev.supportportall.exception.domain.UserNotFoundException;
 import com.atakanoguzdev.supportportall.exception.domain.UsernameExistException;
 import com.atakanoguzdev.supportportall.service.UserService;
+import com.atakanoguzdev.supportportall.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import static com.atakanoguzdev.supportportall.constant.SecurityConstant.JWT_TOKEN_HEADER;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
 public class UserResource extends ExceptionHandling {
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUserName(), user.getPassword());
+        User loginUser = userService.findUserByUserName(user.getUserName());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(user));
+        return headers;
+    }
+
+    private void authenticate(String userName, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName,password));
     }
 
     @PostMapping("/register")
