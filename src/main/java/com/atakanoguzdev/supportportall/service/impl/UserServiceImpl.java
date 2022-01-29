ca856431cf8e7bfa5e5acf28e0ce4e9d29dab9e2
Atakan <atakan17171717@hotmail.com>
2022-01-29 16:43:13 +0300

    login attempt service established contact with other classes

--- src/main/java/com/atakanoguzdev/supportportall/service/impl/UserServiceImpl.java
+++ src/main/java/com/atakanoguzdev/supportportall/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.atakanoguzdev.supportportall.exception.domain.EmailExistException;
 import com.atakanoguzdev.supportportall.exception.domain.UserNotFoundException;
 import com.atakanoguzdev.supportportall.exception.domain.UsernameExistException;
 import com.atakanoguzdev.supportportall.repository.UserRepository;
+import com.atakanoguzdev.supportportall.service.LoginAttemptService;
 import com.atakanoguzdev.supportportall.service.UserService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -23,6 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 import static com.atakanoguzdev.supportportall.constant.UserImplConstant.*;
@@ -36,12 +38,14 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private Logger LOGGER = LoggerFactory.getLogger(getClass());
     private UserRepository userRepository;
     private BCryptPasswordEncoder passwordEncoder;
+    private LoginAttemptService loginAttemptService;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository,BCryptPasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository,BCryptPasswordEncoder passwordEncoder,LoginAttemptService loginAttemptService) {
 
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.loginAttemptService = loginAttemptService;
     }
 
 
@@ -52,6 +56,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             LOGGER.error(NO_USER_FOUND_BY_USERNAME + username);
             throw new UsernameNotFoundException(NO_USER_FOUND_BY_USERNAME + username);
         }else {
+            validateLoginAttempt(user);
             user.setLastLoginDateDisplay(user.getLastLoginDate());
             user.setLastLoginDate(new Date());
             userRepository.save(user);
@@ -60,6 +65,20 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             return userPrincipal;
         }
 
+    }
+
+    private void validateLoginAttempt(User user) {
+        if (user.isNotLocked()){
+            if (loginAttemptService.hasExceededMaxAttempts(user.getUserName())) {
+                user.setNotLocked(false);
+            }
+            else {
+                user.setNotLocked(true);
+            }
+        }
+        else {
+            loginAttemptService.evictUserFromLoginAttemptCache(user.getUserName());
+        }
     }
 
     @Override
