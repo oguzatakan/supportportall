9db85bde196989247c14e6e1f63013b9a4034d95
atakanoguz <atakan17171717@hotmail.com>
2022-02-23 13:19:11 +0300

    Temporary Profıl Image mapping created

--- src/main/java/com/atakanoguzdev/supportportall/resource/UserResource.java
+++ src/main/java/com/atakanoguzdev/supportportall/resource/UserResource.java
@@ -22,11 +22,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+import static com.atakanoguzdev.supportportall.constant.FileConstant.*;
 import static com.atakanoguzdev.supportportall.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
 @RequestMapping(path = {"/","/user"})
@@ -118,6 +125,26 @@ public class UserResource extends ExceptionHandling {
     public ResponseEntity<User> updateProfıleIMage(@RequestParam("username") String username, @RequestParam(value = "profileImage") MultipartFile profileImage) throws UserNotFoundException, EmailExistException, IOException, UsernameExistException {
         User user = userService.updateProfileImage(username,profileImage);
         return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/image/{username}/{fileName}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getProfileImage(@PathVariable("username") String username,@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + fileName));
+    }
+
+    @GetMapping(path = "/image/profile/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while ((bytesRead = inputStream.read(chunk)) > 0){
+                byteArrayOutputStream.write(chunk,0,bytesRead);
+            }
+
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 
     private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
